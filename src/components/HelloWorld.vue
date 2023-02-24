befaf0248af894621b6d77efb264f2a6d9a9b837
Starry29 <18457552195@126.com>
2023-02-25 03:17:51 +0800

    上传dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
 </script>
 
 <template>
@@ -25,7 +26,7 @@ const count = ref(0)
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
-
+  
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
