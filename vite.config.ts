e8e90ead521d7d6e547cbf272630c16ae49572ee
Starry29 <18457552195@126.com>
2023-02-25 03:25:52 +0800

    重新build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen/',
+  base: '/github-page-demo1/dist/',
   plugins: [vue()]
 })
