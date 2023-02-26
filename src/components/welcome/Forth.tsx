d28f3cb95fa79c9a6c681e884dcb308e764aeec2
Starry29 <18457552195@126.com>
2023-02-26 14:43:32 +0800

    四个欢迎页面搞定

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import s from './First.module.scss'
+import cloud from '../../assets/icons/cloud.svg'
+
 export const Forth = defineComponent({
   setup() {
     return () => (
-      <div>Forth</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.icon} src={cloud} />
+          <h2>云备份<br/>再也不怕数据丢失</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+          <RouterLink to="/start">完成</RouterLink>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+        </div>
+      </div>
     )
   }
 })
