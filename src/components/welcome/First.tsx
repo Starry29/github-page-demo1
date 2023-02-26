b3396ebcb1ff8b3770e3085dca962acdd96a1c40
Starry29 <18457552195@126.com>
2023-02-26 15:27:31 +0800

    重构：提取WelcomeLayout组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -2,21 +2,22 @@ import { defineComponent } from 'vue';
 import { RouterLink } from 'vue-router';
 import s from './First.module.scss'
 import pig from '../../assets/icons/pig.svg'
+import {WelcomeLayout} from './WelcomeLayout'
 
 export const First = defineComponent({
-  setup() {
+  setup:(props, context) => {
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.icon} src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-          <RouterLink to="/welcome/2">下一页</RouterLink>
-          <RouterLink to="/start">跳过</RouterLink>
-        </div>
-      </div>
+      <WelcomeLayout>
+        {{
+          icon: () => <img class={s.icon} src={pig} />,
+          title: () => <h2>会挣钱<br/>还要会省钱</h2>,
+          buttons: () => <>
+            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+            <RouterLink to="/welcome/2">下一页</RouterLink>
+            <RouterLink to="/start">跳过</RouterLink>
+          </>
+        }}
+      </WelcomeLayout>
     )
   }
-})
+})  
