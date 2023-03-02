f4fb898a3531e34f1e90390c015d68b4a7cd8a44
Starry29 <18457552195@126.com>
2023-03-02 17:18:23 +0800

    完善登录接口

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,16 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 import { defineComponent, PropType, reactive, ref } from 'vue';
+import { useBool } from '../hooks/useBool';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
+import { history } from '../shared/history';
 import { http } from '../shared/Http';
 import { Icon } from '../shared/Icon';
-import { validate } from '../shared/validate';
+import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
-      email: '',
+      email: 'fangyinghang@foxmail.com',
       code: ''
     })
     const errors = reactive({
@@ -18,7 +20,8 @@ export const SignInPage = defineComponent({
       code: []
     })
     const refValidationCode = ref<any>()
-    const onSubmit = (e: Event) => {
+    const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
+    const onSubmit = async (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
         email: [], code: []
@@ -28,6 +31,11 @@ export const SignInPage = defineComponent({
         { key: 'email', type: 'pattern', regex: /.+@.+/, message: '必须是邮箱地址' },
         { key: 'code', type: 'required', message: '必填' },
       ]))
+      if (!hasError(errors)) {
+        const response = await http.post<{ jwt: string }>('/session', formData)
+        localStorage.setItem('jwt', response.data.jwt)
+        history.push('/')
+      }
     }
     const onError = (error: any) => {
       if (error.response.status === 422) {
@@ -37,9 +45,11 @@ export const SignInPage = defineComponent({
     }
     const onClickSendValidationCode = async () => {
 
+      disabled()
       const response = await http
         .post('/validation_codes', { email: formData.email })
         .catch(onError)
+        .finally(enable)
       // 成功
       refValidationCode.value.startCount()
 
@@ -55,17 +65,19 @@ export const SignInPage = defineComponent({
                 <Icon class={s.icon} name="mangosteen" />
                 <h1 class={s.appName}>山竹记账</h1>
               </div>
+              <div>{JSON.stringify(formData)}</div>
               <Form onSubmit={onSubmit}>
                 <FormItem label="邮箱地址" type="text"
                   placeholder='请输入邮箱，然后点击发送验证码'
                   v-model={formData.email} error={errors.email?.[0]} />
                 <FormItem ref={refValidationCode} label="验证码" type="validationCode"
                   placeholder='请输入六位数字'
-                  countFrom={60}
+                  countFrom={1}
+                  disabled={refDisabled.value}
                   onClick={onClickSendValidationCode}
                   v-model={formData.code} error={errors.code?.[0]} />
                 <FormItem style={{ paddingTop: '96px' }}>
-                  <Button>登录</Button>
+                  <Button type="submit">登录</Button>
                 </FormItem>
               </Form>
             </div>
