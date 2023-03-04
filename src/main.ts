e1a17996deb22ac747743bc46809fadf80a4d1f8
Starry29 <18457552195@126.com>
2023-03-05 04:26:13 +0800

    Pinia 的使用与注意事项

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
+import { useMeStore } from './stores/useMeStore';
 import { routes } from './config/routes';
 import { createApp } from 'vue'
 import { App } from './App'
 import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
-import { fetchMe, mePromise } from './shared/me';
+import { createPinia, storeToRefs } from 'pinia';
+
 
 const router = createRouter({ history, routes })
+const pinia = createPinia()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
 
-fetchMe()
+const meStore = useMeStore()
+const {mePromise} = storeToRefs(meStore)
+meStore.fetchMe()
+
+
 
 const whiteList: Record<string, 'exact' | 'startsWith'> = {
   '/': 'exact',
@@ -27,12 +38,8 @@ router.beforeEach((to, from) => {
       return true
     }
   }
-  return mePromise!.then(
+  return mePromise!.value!.then(
     () => true,
     () => '/sign_in?return_to=' + to.path
   )
 })
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
