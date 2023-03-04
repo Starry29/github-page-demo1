49c705f127edab8a5a2570e4cc642c8aafcb88f0
Starry29 <18457552195@126.com>
2023-03-04 19:52:50 +0800

    Axios实践之记账页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,12 +1,14 @@
 import { AxiosError } from 'axios';
 import { Dialog } from 'vant';
-import { defineComponent, PropType, reactive } from 'vue';
+import { defineComponent, onMounted, PropType, reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
 import { BackIcon } from '../../shared/BackIcon';
+import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
 import { Tabs, Tab } from '../../shared/Tabs';
+import { useTags } from '../../shared/useTags';
 import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 import { Tags } from './Tags';
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, PropType } from 'vue';
 import { RouterView } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 import { Icon } from '../shared/Icon';
 import s from './ItemPage.module.scss';
 export const ItemPage = defineComponent({
@@ -8,9 +9,9 @@ export const ItemPage = defineComponent({
       type: String as PropType<string>
     }
   },
-  setup(props, context) {
+  setup: (props, context) => {
     return () => (
-      <RouterView/>
+      <RouterView />
     )
   }
 })
